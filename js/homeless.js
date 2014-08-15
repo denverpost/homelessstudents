96b71ef7ced97c0e31fb5f594da52aa8dda9ff54
schneidan <dan@scrollwright.com>
2014-08-15 17:13:27 -0600

    Completed second-title-slide JS and CSS; inset photo styles; lots of typography refining.

--- js/homeless.js
+++ js/homeless.js
@@ -5,11 +5,20 @@ function revealBio(bioname) {
 	$(bioname).foundation('reveal', 'open');
 }
 
+function getNodePosition(node) {
+	var eTop = $(node).offset().top;
+	return Math.abs(eTop - $(window).scrollTop());
+}
+
 $(window).scroll(function() {
 	var s = $(window).scrollTop();
+	var t = getNodePosition('.opener.opener2');
 	var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 	opacityNew = s / h;
+	opacityNewTwo = t / h;
+	console.log(1 - opacityNewTwo);
 	$(".opener.opener1 div#title").children().css("opacity", 1 - opacityNew);
+	$(".opener.opener2 div.title-below").children().css("opacity", 1 - opacityNewTwo);
 	$('#name1').css("opacity", 1 - opacityNew*.75);
 	$('#name2').css("opacity", opacityNew*.75);
 });
