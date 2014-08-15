303689f75184f7288c5c3e1f09042defd3a3ad8d
schneidan <dan@scrollwright.com>
2014-08-14 18:05:43 -0600

    Typography tweaks, evening wrapup.

--- js/homeless.js
+++ js/homeless.js
@@ -5,27 +5,13 @@ function revealBio(bioname) {
 	$(bioname).foundation('reveal', 'open');
 }
 
-/*function titleChange() {
-$('div.opener').bind('inview', function(event, visible) {
-	if (visible) {
-		$(this).stop().animate({ opacity: 1}, 1500);
-	}
-	if (!visible) {
-		$('#name1').animate({opacity: 0}, 450);
-		$('#name2').animate({opacity: 1}, 450);
-	} else {
-		$('#name1').stop().animate({opacity: 1}, 150);
-		$('#name2').stop().animate({opacity: 0}, 150);
-	}
-})};
-derp1(); */
-
 $(window).scroll(function() {
-	var s = $(window).scrollTop(),
-	opacityNew = s / 500;
+	var s = $(window).scrollTop();
+	var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+	opacityNew = s / h;
 	$(".opener.opener1 div#title").children().css("opacity", 1 - opacityNew);
-	$('#name1').css("opacity", 1 - opacityNew*.5);
-	$('#name2').css("opacity", opacityNew*.5);
+	$('#name1').css("opacity", 1 - opacityNew*.75);
+	$('#name2').css("opacity", opacityNew*.75);
 });
 
 $(document).ready(function(){
