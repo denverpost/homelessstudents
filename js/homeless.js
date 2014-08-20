020e564b3a3298083f4742079f709b6f04d0a079
schneidan <dan@scrollwright.com>
2014-08-20 16:18:00 -0600

    Bar charts are IN! Edits and tweaks throughout.

--- js/homeless.js
+++ js/homeless.js
@@ -74,3 +74,99 @@ function showAd() {
         moreAd = false;
     }
 }
+
+function showNumberChart() {
+	$('#high_chart_div_wrap').fadeOut(300);
+	$('#large_chart_div_wrap').fadeIn(300);
+	$('.percentLink').removeClass('livechart');
+	$('.numberLink').addClass('livechart');
+}
+function showPercentChart() {
+	$('#large_chart_div_wrap').fadeOut(300);
+	$('#high_chart_div_wrap').fadeIn(300);
+	$('.numberLink').removeClass('livechart');
+	$('.percentLink').addClass('livechart');
+}
+
+function executeAsynchronously(functions, timeout) {
+  for(var i = 0; i < functions.length; i++) {
+    setTimeout(functions[i], timeout);
+  }
+}
+function loadFirstChart() {
+	google.load("visualization", "1", {packages:["corechart"]});
+	google.setOnLoadCallback(drawChart);
+	function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+			['District', '2004', '2013'],
+			  ["Denver",753,1066],
+			["Jefferson",717,1731],
+			["Douglas",0,590],
+			["Cherry Creek",7,52],
+			["Adams 12",281,1055],
+			["Aurora",237,2300],
+			["Boulder",123,400],
+			["St. Vrain",391,1060],
+			["Poudre",326,588],
+			["Colo. Springs",79,437],
+			//["Academy 20",75,12],
+			//["Mesa County Valley",155,151],
+			//["Greeley",1073,61],
+			//["Falcon",47,1],
+			//["Pueblo City",1108,940]
+		]);
+
+        var options = {
+			title: 'Large School Districts: Number of Homeless Students',
+			legend: { position: 'top' },
+			/* bar: {groupWidth: '80%' }, */
+			isStacked: true,
+			hAxis: { textPosition: 'none' },
+			vAxis: { textPosition: 'out' },
+			vAxis: { textStyle: {fontSize: 12, bold:true} },
+			//vAxis: {title: 'Year',  titleTextStyle: {color: 'red'}}
+			series: { 0: { color: 'lightgray'}, 1: { color: 'steelblue' }}
+        };
+
+        var chart = new google.visualization.BarChart(document.getElementById('large_chart_div'));
+
+        chart.draw(data, options);
+	}
+}
+function loadSecondChart() {
+	google.load("visualization", "1", {packages:["corechart"]});
+	google.setOnLoadCallback(drawChart);
+	function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+			['District', 'Pct. Homeless 2004', 'Pct. Homeless 2013'],
+			["Mtn. Valley",5.3,21.5],
+			["Mtn. BOCES",0,15.3],
+			["Sheridan",4.5,14.7],
+			["Center",0.3,13.4],
+			["Hanover",17.1,11.2],
+			["Moffat",0,11.1],
+			["Cripple Creek",4.5,8.7],
+			["Westminster",6.7,8.3],
+			["Del Norte",0,6.8],
+			["Adams 14",0.2,6.7]
+		]);
+
+        var options = {
+			title: 'School Districts with Highest Percentage of Homeless Students',
+			legend: { position: 'top' },
+			/*bar: {groupWidth: '80%' }, */
+			isStacked: true,
+			hAxis: { textPosition: 'none' },
+			vAxis: { textPosition: 'out' },
+			vAxis: { textStyle: {fontSize: 12, bold:true} },
+			//vAxis: {title: 'Year',  titleTextStyle: {color: 'red'}}
+			series: { 0: { color: 'lightgray'}, 1: { color: 'steelblue' }}
+        };
+
+        var chart = new google.visualization.BarChart(document.getElementById('high_chart_div'));
+
+        chart.draw(data, options);
+	}
+}
+loadFirstChart();
+loadSecondChart();
